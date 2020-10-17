bf3029027d83b0ddf21b46874af2fdf9ab7f1bd0
Adam <afekaoh@gmail.com>
2020-10-17 05:32:33 -0700

    cleaning up and adding distinctions between adam and oz

--- cmake-build-debug/CMakeFiles/AdvanceProg1.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/AdvanceProg1.dir/cmake_clean.cmake
@@ -1,9 +1,7 @@
 file(REMOVE_RECURSE
-  "AdvanceProg1.exe"
+  "AdvanceProg1"
   "AdvanceProg1.pdb"
-  "CMakeFiles/AdvanceProg1.dir/ass1/MainTrain.cpp.o"
-  "CMakeFiles/AdvanceProg1.dir/ass1/anomaly_detection_util.cpp.o"
-  "libAdvanceProg1.dll.a"
+  "CMakeFiles/AdvanceProg1.dir/main.cpp.o"
 )
 
 # Per-language clean rules from dependency scanning.
