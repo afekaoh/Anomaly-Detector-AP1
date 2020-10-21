897387942472516d6f9336f65e14f9ea8a40f13f
Oz Rigler <ozzri28@gmail.com>
2020-10-21 15:32:24 +0300

    first 3 stages of ht project

--- ass1-oz/MainTrain.cpp
+++ ass1-oz/MainTrain.cpp
@@ -19,13 +19,8 @@ int main() {
 	const int N = 10;
 	float x[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	float y[] = {2.1, 4.2, 6.1, 8.1, 10.3, 12.2, 14.4, 16.1, 18.2, 20.3};
-	cout << cov(x, y, N) << endl;
-	cout << var(x, N) << endl;
-	cout << var(y, N) << endl;
-	cout << pearson(x,y, N) << endl;
 
-
-	Point *ps[N];
+	Point* ps[N];
 	for (int i = 0; i < N; i++)
 		ps[i] = new Point(x[i], y[i]);
 
--- ass1-oz/anomaly_detection_util.cpp
+++ ass1-oz/anomaly_detection_util.cpp
@@ -2,14 +2,15 @@
  * animaly_detection_util.cpp
  *
  * Author: write your ID and name here
+ * oz rigler 316291897
  */
 
 #include <math.h>
 #include <iostream>
 #include "anomaly_detection_util.h"
 
-float avg(float *x, int size) {
-	float sum;
+float avg(float* x, int size) {
+	float sum = 0;
 	for (int i = 0; i < size; ++i) {
 		sum += x[i];
 	}
@@ -17,9 +18,9 @@ float avg(float *x, int size) {
 }
 
 // returns the variance of X and Y
-float var(float *x, int size) {
+float var(float* x, int size) {
 	float average = avg(x, size);
-	float varianceSum;
+	float varianceSum = 0;
 	for (int i = 0; i < size; ++i) {
 		varianceSum += powf(x[i] - average, 2);
 	}
@@ -27,47 +28,51 @@ float var(float *x, int size) {
 }
 
 // returns the covariance of X and Y
-float cov(float *x, float *y, int size) {
+float cov(float* x, float* y, int size) {
 	float averageX = avg(x, size);
 	float averageY = avg(y, size);
 	float covariances[size];
 	for (int i = 0; i < size; ++i) {
 		covariances[i] = (x[i] - averageX) * (y[i] - averageY);
 	}
-	float covarianceSum;
+	float covarianceSum = 0;
 	for (int i = 0; i < size; ++i) {
 		covarianceSum += covariances[i];
 	}
 	return covarianceSum / (float) size;
 }
 
-void printArr(float *x, int size) {
-	for (int i = 0; i < size; ++i) {
-		std::cout << x[i] << " ";
-	}
-	std::cout << std::endl;
-}
-
 // returns the Pearson correlation coefficient of X and Y
-float pearson(float *x, float *y, int size) {
+float pearson(float* x, float* y, int size) {
 	return (cov(x, y, size) / (sqrt(var(x, size) * var(y, size))));
 }
 
 
 // performs a linear regression and returns the line equation
-Line linear_reg(Point **points, int size) {
+Line linear_reg(Point** points, int size) {
+	float x[size];
+	float y[size];
+	for (int i = 0; i < size; ++i) {
+		x[i] = points[i]->x;
+		y[i] = points[i]->y;
+	}
+	float slope = cov(x, y, size) / var(x, size);
+	float xAvrg = avg(x, size);
+	float yAvrg = avg(y, size);
 
-	return Line(0, 0);
+	return Line(slope, yAvrg - slope * xAvrg);
 }
 
 // returns the deviation between point p and the line equation of the points
-float dev(Point p, Point **points, int size) {
-	return 0;
+float dev(Point p, Point** points, int size) {
+	Line line = linear_reg(points, size);
+	return dev(p, line);
 }
 
 // returns the deviation between point p and the line
 float dev(Point p, Line l) {
-	return 0;
+	float distance = fabs(l.f(p.x) - p.y);
+	return distance;
 }
 
 
